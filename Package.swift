5975fd54275f9a7c472a34122bd1833da8e9d85f
joshlacal <joshlacal@gmail.com>
2024-10-19 16:38:37 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,8 @@
 // swift-tools-version: 5.8.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
-    name: "jose-swift",
+    name: "JoseSwift",
     platforms: [
         .iOS(.v14),
         .macOS(.v12),
